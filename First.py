79113777bcb8738a4fd6cc606681a78eb7790c91
Yumnah Jaleel <gq4101@myamu.ac.in>
2026-01-29 16:51:45 +0530

    Add files

---
+++ First.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+x = 5
+y = "Yumnah"
+Lesson = "Python is dynamic typed language\nit means you don't need to declare variable type"
+also = "It has automatic garbage collection"
+print(x)
+print(y)
+print(Lesson)
+print(also)
---
+++ Second.py
@@ -0,0 +1,3 @@
+# hello
+print(9*6 )
+""" doc string """
