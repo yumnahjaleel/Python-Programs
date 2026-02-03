7032a6305046c63da6f38de30bc1a25669f005cb
Yumnah Jaleel <gq4101@myamu.ac.in>
2026-02-03 11:55:49 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,15 @@
+x = 10
+y= 12
+w = 4
+x = 9
+NME = "Hello"
+NME = "HH"
+print(x + y + w)
+print(w)
+print(x)
+print(NME)
+
+price = 100
+tax = 0.08
+total_price = price + (price * tax)
+print(total_price)
